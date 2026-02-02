ec4eea98881ccb555e74689d549b3ac333367616
Akshanabha Chakraborty <ahqsecret@gmail.com>
2026-02-02 07:47:48 +0530

    Update build.rs to support aarch64 architecture

--- build.rs
+++ build.rs
@@ -13,22 +13,5 @@ fn main() {
       )
     )
   )]
-  compile_error!("This package only works for x86_64 Windows and Linux runners");
-
-
-  #[cfg(
-    all(
-      not(feature = "aarch64"),
-      target_arch = "aarch64"
-    )
-  )]
-  compile_error!("Arm64 support is currently experimental! Unless you know what you are doing, enable the `aarch64` feature");
-
-  #[cfg(
-    all(
-      feature = "aarch64",
-      target_arch = "aarch64"
-    )
-  )]
-  println!("cargo:warning=`aarch64` JIT Loader is untested, it should work.");
+  compile_error!("This package only works for x86_64 and aarch64 Windows and Linux runners");
 }
