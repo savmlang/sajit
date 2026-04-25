d2477fc3258da3e453e15d42fe4a7ffb9d5fb9c6
Akshanabha Chakraborty <ahqsecret@gmail.com>
2026-04-25 23:08:43 +0530

    JITUpdate

--- src/lib.rs
+++ src/lib.rs
@@ -47,7 +47,59 @@ unsafe fn relocate(mmap: *mut u8, len: usize, relocation: &Relocation) {
   // wrapping_add ensures that stuff becomes negative
   let value = (relocation.symbol_addr as u64).wrapping_add(relocation.addend.cast_unsigned());
 
+  #[cfg(target_arch = "x86_64")]
+  let x86callhwnd = |displacement: i64| {
+    debug_assert!((relocation.offset as usize + 4) <= len);
+    #[cfg(debug_assertions)]
+    if displacement > i32::MAX as _ || displacement < i32::MIN as _ {
+      panic!("Relocation truncated to fit: Target is too far for 32-bit offset");
+    }
+
+    let displacement_32 = displacement as i32;
+    unsafe {
+      // SAFETY:
+      // x86_64 CPUs tolerate unaligned access, so this is okay even if the pointer is not 4-byte aligned.
+      //
+      // In practice, with Mmap and proper offsets, this pointer is aligned anyway.
+      std::ptr::copy_nonoverlapping(
+        &displacement_32 as *const i32 as *const u8,
+        patch_site as *mut u8,
+        4,
+      );
+    }
+  };
+
+  #[cfg(target_arch = "aarch64")]
+  let arm64callhwnd = |displacement_bytes: i64| {
+    debug_assert_eq!(
+      displacement_bytes % 4,
+      0,
+      "ARM64 branch target must be 4-byte aligned"
+    );
+
+    // OPTIMIZATION
+    // Directly do division by `4`
+    let displacement = displacement_bytes >> 2;
+
+    debug_assert!((relocation.offset as usize + 4) <= len);
+
+    #[cfg(debug_assertions)]
+    if displacement > 0x1FFFFFF || displacement < -0x2000000 {
+      panic!("Relocation truncated: Target is too far for ARM64 26-bit offset");
+    }
+
+    unsafe {
+      let mut instruction = ptr::read_unaligned(patch_site as *const u32);
+
+      instruction &= 0xFC000000;
+      instruction |= (displacement as u32) & 0x03FFFFFF;
+
+      ptr::write_unaligned(patch_site as *mut u32, instruction);
+    }
+  };
+
   match relocation.kind {
+    #[cfg(target_pointer_width = "64")]
     RelocKind::Abs8 => unsafe {
       debug_assert_eq!(relocation.addend, 0);
       debug_assert!((relocation.offset as usize + 8) <= len);
@@ -56,15 +108,19 @@ unsafe fn relocate(mmap: *mut u8, len: usize, relocation: &Relocation) {
     },
     #[cfg(target_pointer_width = "64")]
     RelocKind::Abs4 => unimplemented!("Unsupported platform"),
+    #[cfg(target_pointer_width = "32")]
+    RelocKind::Abs8 => unimplemented!("Unsupported platform"),
     #[cfg(not(target_pointer_width = "64"))]
     RelocKind::Abs4 => unsafe {
       debug_assert_eq!(relocation.addend, 0);
       debug_assert!((relocation.offset as usize + 4) <= len);
 
       ptr::write_unaligned(patch_site as *mut u32, value as u32);
-    }
+    },
     #[cfg(not(target_arch = "x86_64"))]
-    RelocKind::X86CallPCRel4 | RelocKind::X86PCRel4 => unimplemented!("Unsupported platform"),
+    RelocKind::X86CallPCRel4
+    | RelocKind::X86PCRel4
+    | RelocKind::X86CallPCRelOrPCRelProvidedRelativeBytes => unimplemented!("Unsupported platform"),
     #[cfg(target_arch = "x86_64")]
     RelocKind::X86CallPCRel4 | RelocKind::X86PCRel4 => {
       let target_addr = value as i64;
@@ -72,58 +128,27 @@ unsafe fn relocate(mmap: *mut u8, len: usize, relocation: &Relocation) {
 
       let displacement = target_addr.wrapping_sub(rip_after_instr);
 
-      debug_assert!((relocation.offset as usize + 4) <= len);
-      #[cfg(debug_assertions)]
-      if displacement > i32::MAX as _ || displacement < i32::MIN as _ {
-        panic!("Relocation truncated to fit: Target is too far for 32-bit offset");
-      }
-
-      let displacement_32 = displacement as i32;
-      unsafe {
-        // SAFETY:
-        // x86_64 CPUs tolerate unaligned access, so this is okay even if the pointer is not 4-byte aligned.
-        //
-        // In practice, with Mmap and proper offsets, this pointer is aligned anyway.
-        std::ptr::copy_nonoverlapping(
-          &displacement_32 as *const i32 as *const u8,
-          patch_site as *mut u8,
-          4,
-        );
-      }
+      x86callhwnd(displacement);
+    }
+    #[cfg(target_arch = "x86_64")]
+    RelocKind::X86CallPCRelOrPCRelProvidedRelativeBytes => {
+      x86callhwnd(relocation.symbol_addr.cast_signed());
     }
     #[cfg(not(target_arch = "aarch64"))]
-    RelocKind::Arm64Call => unimplemented!("Unsupported platform"),
+    RelocKind::Arm64Call | RelocKind::Arm64CallProvidedRelativeBytes => {
+      unimplemented!("Unsupported platform")
+    }
     #[cfg(target_arch = "aarch64")]
     RelocKind::Arm64Call => {
       let target_addr = value as i64;
       let rip_after_instr = patch_site as i64;
       let displacement_bytes = target_addr.wrapping_sub(rip_after_instr);
 
-      debug_assert_eq!(
-        displacement_bytes % 4,
-        0,
-        "ARM64 branch target must be 4-byte aligned"
-      );
-
-      // OPTIMIZATION
-      // Directly do division by `4`
-      let displacement = displacement_bytes >> 2;
-
-      debug_assert!((relocation.offset as usize + 4) <= len);
-
-      #[cfg(debug_assertions)]
-      if displacement > 0x1FFFFFF || displacement < -0x2000000 {
-        panic!("Relocation truncated: Target is too far for ARM64 26-bit offset");
-      }
-
-      unsafe {
-        let mut instruction = ptr::read_unaligned(patch_site as *const u32);
-
-        instruction &= 0xFC000000;
-        instruction |= (displacement as u32) & 0x03FFFFFF;
-
-        ptr::write_unaligned(patch_site as *mut u32, instruction);
-      }
+      arm64callhwnd(displacement_bytes);
+    }
+    #[cfg(target_arch = "aarch64")]
+    RelocKind::Arm64Call => {
+      arm64callhwnd(relocation.symbol_addr.cast_signed());
     }
   }
 }
--- src/relocations/mod.rs
+++ src/relocations/mod.rs
@@ -14,20 +14,39 @@ pub enum RelocKind {
   Abs8 = 0,
 
   /// This describes a signed 32-bit relative offset call (+- 2GiB)
+  ///
+  /// `symbol_addr` still corresponds to absolute address - we calculate
+  /// displacement
   X86CallPCRel4 = 1,
   /// This describes a signed 32-bit relative offset call (+- 2GiB)
+  ///
+  /// `symbol_addr` still corresponds to absolute address - we calculate
+  /// displacement
   X86PCRel4 = 2,
+
+  /// This describes a signed 32-bit relative offset call (+- 2GiB)
+  ///
+  /// `symbol_addr` corresponds to **relative displacement**
+  /// and is treated as i64 not u64
+  ///
+  /// `addend` is ignored
+  X86CallPCRelOrPCRelProvidedRelativeBytes = 3,
+
   // Aarch64
   /// This is the arm64 24-bit relative call (+-128MiB)
-  Arm64Call = 3,
+  Arm64Call = 4,
+
+  // Aarch64
+  /// This is the arm64 24-bit relative call (+-128MiB)
+  ///
+  /// Unlike `Arm64Call`, `symbol_addr` corresponds to **relative byte
+  /// displacement**
+  ///
+  /// `addend` is ignored
+  Arm64CallProvidedRelativeBytes = 5,
+
   // X86
   /// This describes an Intel `i686` 8-bit absolute address
   /// Since intel x86 are 32-bit, this is the only one required
-  Abs4 = 4,
-  // Cranelift doesn't generate these for
-  // function calls
-  //
-  // Aarch64AdrGotPage21 = 4,
-  // Aarch64Ld64GotLo12Nc = 5,
-  // Aarch64AddAbsLo12Nc = 6,
+  Abs4 = 6,
 }
