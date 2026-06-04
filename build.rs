7d11074b7203badfc66e362f14eb1c36fe1bbfd0
Akshanabha Chakraborty <ahqsecret@gmail.com>
2026-06-04 20:49:37 +0530

    Disable RTTI

--- build.rs
+++ build.rs
@@ -78,7 +78,7 @@ fn jitlink_llvm() {
 
   let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
 
-  if target_os == "linux"
+  if target_os == "linux" || target_os == "darwin" || target_os == "macos"
   {
     build.flag("-fno-rtti");
   }
